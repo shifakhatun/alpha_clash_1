3e6488dae51e3b4bb4c61e6a0104cd7d8e0ddea6
shifa <shifa.eee@gmail.com>
2024-02-14 18:52:49 +0600

    Random Alphabet added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,5 +1,33 @@
+// //function play(){
+//     //hide the home screen..to hide the screen add the hidden class to the home section
+  
+//     //console.log('play start now');
+//     const homeSection=document.getElementById("home-screen")
+//     homeSection.classList.add('hidden')
+//     //console.log(homeSection.classList);
+
+//       //show the playground
+//       const playGroundSection=document.getElementById('play-ground')
+//       playGroundSection.classList.remove('hidden')
+//       console.log(playGroundSection.classList)
+// }
+function continueGame(){
+        //step 1;  generate a  random alphabet
+        //
+       const alphabet=getARandomAlphabet();
+       console.log('your random alphabet', alphabet);
+
+
+       //set randomly generated alphabet to the screen
+       const currentAlphabetElement=document.getElementById('current-alphabet');
+       currentAlphabetElement.innerText=alphabet;
+
+}
+
+
 function play(){
-    //hide the home screen..to hide the screen add the hidden class to the home section
-    //show the playground
-    //console.log('play start now');
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
+
 }
